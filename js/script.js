69a5cbcb249223f9bd31a73e1a38c0c2b05925be
CretiveAcademyIndonesia <agus.sugandi95@gmail.com>
2021-11-20 22:03:07 +0700

    menambahkan interaktifitas navbar

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const navbar = document.getElementsByTagName('nav')[0];
+window.addEventListener('scroll', function() {
+    if ( window.scrollY > 1 ) {
+    navbar.classList.replace('bg-transparent','nav-color');
+} else if (window.scrollY <= 1) {
+    navbar.classList.replace('nav-color','bg-transparent');
+}
+});
