7cdf6e65902a666e3dc4abc43b3ba7b900c77e74
CretiveAcademyIndonesia <agus.sugandi95@gmail.com>
2021-11-20 22:16:31 +0700

    oke

--- js/script.js
+++ js/script.js
@@ -1,32 +1,17 @@
-// const navbar = document.getElementsByTagName('nav')[0];
-// window.addEventListener('scroll', function() {
-//     if ( window.scrollY > 1 ) {
-//     navbar.classList.replace('bg-transparent','nav-color');
-//     } else if (window.scrollY <= 1) {
-//         navbar.classList.replace('nav-color','bg-transparent');
-//     }
-// });
-
-// document.body.addEventListener('touchmove', function() {
-//     if ( window.scrollY > 1 ) {
-//     navbar.classList.replace('bg-transparent','nav-color');
-//     } else if (window.scrollY <= 1) {
-//         navbar.classList.replace('nav-color','bg-transparent');
-//     }
-// });
+const navbar = document.getElementsByTagName('nav')[0];
+window.addEventListener('scroll', function() {
+    if ( window.scrollY > 1 ) {
+    navbar.classList.replace('bg-transparent','nav-color');
+    } else if (window.scrollY <= 1) {
+        navbar.classList.replace('nav-color','bg-transparent');
+    }
+});
 
 const body = document.getElementsByTagName('body')[0];
 body.addEventListener('touchmove', function() {
-    alert('ok');
+    if ( window.scrollY > 1 ) {
+        navbar.classList.replace('bg-transparent','nav-color');
+    } else if (window.scrollY <= 1) {
+        navbar.classList.replace('nav-color','bg-transparent');
+    }
 });
-
-// $(document.body).on('touchmove', onScroll); // for mobile
-// $(window).on('scroll', onScroll); 
-
-// // callback
-// function onScroll(){ 
-//     if( $(window).scrollTop() + window.innerHeight >= document.body.scrollHeight ) { 
-//         track_page++; 
-//         load_contents(track_page); 
-//     }
-// }
